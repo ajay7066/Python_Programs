e997a893ccc2fb1c1b429e534c3e719e87a093c5
AJAY EKNATH MUTHAL <ajjumuthal07@gmail.com>
2022-10-02 18:27:09 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,9 +1,17 @@
-def factorial():
-	num = int(input("enter a number : "))
+def factorial(num):
+	# num = int(input("enter a number : "))
 	if(num == 1):
 		return 1
 
 	else:
-		return factorial(factorial*(num-1))
+		return num*factorial(num-1)
+num = 6
 
-factorial()
+if num<0:
+	print("The number is negative")
+
+elif num == 0:
+	print(num,"'s factorial is 1")
+
+else:
+	print(num,"factorial is ",factorial(num))
